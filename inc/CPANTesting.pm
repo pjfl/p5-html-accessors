d4ba3717aa37a813b8baa5ca54617fd644994129
pjf <pjf@0da11a13-f23a-47a8-b648-f8e5dc219371>
2012-05-07 16:55:44 +0000

    Create CPAN testing exceptions for the usual suspects because of 860317fe-771b-1014-bd10-14ac237ab312. Honestly, how fucked up does CPAN testing have to get before something is done to improve the quality of the results

--- inc/CPANTesting.pm
+++ inc/CPANTesting.pm
@@ -5,13 +5,17 @@ package CPANTesting;
 use strict;
 use warnings;
 
-my $uname = qx(uname -a);
+my $osname = lc $^O; my $uname = lc qx(uname -a);
 
 sub broken_toolchain {
    return 0;
 }
 
 sub exceptions {
+   $osname eq q(cygwin)  and return 'Cygwin not supported';
+   $osname eq q(mirbsd)  and return 'Mirbsd not supported';
+   $osname eq q(mswin32) and return 'Mswin  not supported';
+   $osname eq q(netbsd)  and return 'Netbsd not supported';
    return 0;
 }
 
